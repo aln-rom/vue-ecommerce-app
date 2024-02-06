f90587212161bbf044865c8d6e268c82c199e25b
aln-rom <57873642+aln-rom@users.noreply.github.com>
2024-02-06 09:32:59 +0300

    integrate logo and button in header

--- src/widgets/header/ui.vue
+++ src/widgets/header/ui.vue
@@ -1,13 +1,44 @@
 <script lang="ts" setup>
 import { Container } from "@/shared/container";
+import { Logo } from '@/shared/logo';
+import { Button } from '@/shared/button';
+
 </script>
 
 <template>
   <header class="header">
-    <Container/>
+    <Container class="header__container">
+      <div class="header__container__logo">
+        <RouterLink to="/">
+          <Logo orientation="horizontal" bgColor="white" colorful with-text/>
+        </RouterLink>
+      </div>
+      
+      <div class="header__container__button">
+        <Button color="secondary">
+          Каталог
+        </Button>
+      </div>
+    </Container>
   </header>
 </template>
 
-<style scoped>
-
+<style scoped lang="scss">
+.header {
+  position: sticky;
+  top: 0;
+  left: 0;
+  z-index: 1;
+  
+  &__container {
+    display: flex;
+    align-items: center;
+    height: 72px;
+    
+    &__button {
+      width: 140px;
+      margin-left: 40px;
+    }
+  }
+}
 </style>
